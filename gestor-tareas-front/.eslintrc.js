65d9edde7fc1b16b25f0faee5e00a6401fdf060e
Yoriel <yncarvajalino@unicesar.edu.co>
2025-08-29 14:13:32 -0500

    fixed

--- gestor-tareas-front/.eslintrc.js
+++ gestor-tareas-front/.eslintrc.js
@@ -1,24 +1,27 @@
 module.exports = {
-    root: true,
-    env: {
-      node: true,
-    },
-    extends: [
-      'plugin:vue/vue3-essential',
-      'eslint:recommended'
-    ],
-    parserOptions: {
-      ecmaVersion: 2020,
-    },
-    rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'vue/multi-word-component-names': 'off' // Opcional: permite nombres de componente de una palabra
-    },
-    globals: {
-      defineProps: 'readonly',
-      defineEmits: 'readonly',
-      defineExpose: 'readonly',
-      withDefaults: 'readonly'
-    }
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+
+
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off'
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
   }
+}
